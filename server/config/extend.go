de3b0ce5db132b1ac9bf906a404a8532b270ca78
zhangyunfei1 <zhangyunfei1@100tal.com>
2021-10-15 16:27:20 +0800

    代码整理

--- server/config/extend.go
+++ server/config/extend.go
@@ -8,7 +8,7 @@ var ExtConfig Extend
 //      name: demo-name
 // 使用方法： config.ExtConfig......即可！！
 type Extend struct {
-	AMap AMap   // 这里配置对应配置文件的结构即可
+	AMap AMap // 这里配置对应配置文件的结构即可
 }
 
 type AMap struct {
